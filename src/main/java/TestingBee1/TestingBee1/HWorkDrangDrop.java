c731c50a93196c74a215623c521641d755722816
Mahbuba Yeasmin <mahbuba.yeasmin@pearson.com>
2022-08-22 23:08:22 -0400

    drag and drop

---
+++ src/main/java/TestingBee1/TestingBee1/HWorkDrangDrop.java
@@ -0,0 +1,44 @@
+package TestingBee1.TestingBee1;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HWorkDrangDrop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(6000l, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3000l, TimeUnit.SECONDS);
+		driver.get("https://jqueryui.com/droppable/");
+		WebElement frame=driver.findElement(By.tagName("iframe"));
+		List<WebElement> frames=driver.findElements(By.tagName("iframe"));
+		frames.size();
+		System.out.println(frames.size());
+		driver.switchTo().frame(frame);
+		
+		WebElement source=driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement target=driver.findElement(By.xpath("//div[@id='droppable']"));
+		Actions ac= new Actions(driver);
+		ac.dragAndDrop(source, target).build().perform();
+		driver.switchTo().defaultContent();
+		driver.findElement(By.xpath("//a[@href='http://jqueryui.com/draggable/']")).click();
+		
+	    
+	    
+	
+	}
+
+}
